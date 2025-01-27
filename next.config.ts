4dd4af2fcbc08f5a8d4575b5b0e7acdcb604b948
kyleotero <kkyleotero@gmail.com>
2025-01-26 22:56:51 -0500

    fix pipeline i hope

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
-import type { NextConfig } from "next";
-
 const nextConfig = {
-  basePath: "/",
   output: "export",
   reactStrictMode: true,
+  images: {
+    unoptimized: true, 
+  },
 };
 
 module.exports = nextConfig;
